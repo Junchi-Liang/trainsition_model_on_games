20135303e59b538051bba052ca60fbcef81d9133
Junchi-Liang <junchiliang@163.com>
2017-05-26 17:21:15 -0400

    polynomial fit

--- python/py_misc/gunnar_optical_flow/optical_flow.py
+++ python/py_misc/gunnar_optical_flow/optical_flow.py
@@ -22,7 +22,7 @@ def normal_filter(size, covariance):
 
 def polynomial_fit(w, x, y):
     """
-        fit (x^T)Ax + (B^T)x + C
+        fit polynomial (x^T)Ax + (B^T)x + C
         w : np.array
         w - weights for each sample, 1d array
         x : np.array
@@ -30,7 +30,7 @@ def polynomial_fit(w, x, y):
         y : np.array
         y - intensities of each pixels, 1d array
         return [A, B, C]
-        A : np.arrray
+        A : np.array
         A - coefficient, shape (2, 2)
         B: np.array
         B - coefficient, shape (2, 1)
@@ -57,3 +57,40 @@ def polynomial_fit(w, x, y):
     C = beta[5, 0]
     return [A, B, C]
 
+def collect_pixels(img, p_x, p_y, size, covariance):
+    """
+        form dataset for polynomial fit
+        img : np.array
+        img - input image, 2d array
+        p_x : int
+        p_x - location of the center pixel
+        p_y : int
+        p_y - location of the center pixel
+        size : int
+        size - the size for the filter
+        covariance : array_like
+        covariance - covariance matrix for the filter
+        return [w, x, y]
+        w : np.array
+        w - weights for each sample, 1d array
+        x : np.array
+        x - positions of pixels, its shape should be (2, n)
+        y : np.array
+        y - intensities of each pixels, 1d array       
+    """
+    w_filter = normal_filter(size, covariance)
+    x_list = []
+    y_list = []
+    w_list = []
+    for i in range(-size, size + 1):
+        for j in range(-size, size + 1):
+            x1 = p_x + i
+            x2 = p_y + j
+            if (x1 >= 0 and x1 < img.shape[0] and x2 >= 0 and x2 < img.shape[1]):
+                x_list.append([x1, x2])
+                y_list.append(img[x1, x2])
+                w_list.append(w_filter[i + size, j + size])
+    x = (np.asarray(x_list, np.float32)).transpose()
+    w = np.asarray(w_list, np.float32)
+    y = np.asarray(y_list, np.float32)
+    return [w, x, y]
