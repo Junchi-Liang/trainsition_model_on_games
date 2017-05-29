1c855b437effee4b92162c7ec1545987af77e080
Junchi-Liang <junchiliang@163.com>
2017-05-29 17:19:55 -0400

    helper functions for demonstration

--- python/py_misc/gunnar_optical_flow/drawing.py
+++ python/py_misc/gunnar_optical_flow/drawing.py
@@ -4,6 +4,7 @@ import scipy.misc
 
 def circle_object(p_x, p_y, c, img_height, img_width):
     """
+        generate a picture with a circle
         p_x : int
         p_x - center of the circle
         p_y : int
@@ -26,4 +27,23 @@ def circle_object(p_x, p_y, c, img_height, img_width):
                 if (p_x + i >= 0 and p_x + i < img_height and p_y + j >= 0 and p_y + j < img_width):
                     output[i + p_x, j + p_y] = 255
     return output
-    
+
+def random_motion_pictures(speed_max, c, img_height, img_width):
+    """
+        generate two consecutive image of an moving object
+        img_height : int
+        img_height - height of the image
+        img_width : int
+        img_width - width of the image
+        speed_max : int
+        speed_max - |x1 - x1'| < speed_max and |x2 - x2'| < speed_max
+        c : int
+        c : radius of the object
+        return [img_prev, img_next]
+        img_prev : np.ndarray
+        img_prev - first image
+        img_next : np.ndarray
+        img_next - next image
+    """
+    x1 = np.random.choice(img_height, 1)[0]
+    x2 = np.random.choice(img_width, 1)[0]
