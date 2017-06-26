8b70f13ee93ac8f38cf71ed192fa974a7e100b4d
Junchi-Liang <junchiliang@163.com>
2017-06-26 16:33:09 -0400

    codes for VGG

--- python/my_vgg/vgg_model.py
+++ python/my_vgg/vgg_model.py
@@ -25,3 +25,22 @@ class VGG_model:
         self.train_batch_size = training_batch_size
         self.test_batch_size = test_batch_size
 
+    def construct_network_computation_graph(input_layer = None, batch_size = 0, shared_weight = None):
+        """
+            construct computation graph of Tensorflow for the networks
+            input_layer : tensorflow.python.framework.ops.Tensor
+            input_layer = input layer. If it is None, a new layer of placeholder will be constructed
+            batch_size : int
+            batch_size = batch size. If an input layer is provided, this argument will be omitted
+            shared_weight : dictionary
+            shared_weight = weights for each layers. If it is None, new weights will be constructed
+            ---------------------------------------------------------------------------------------
+            return [layers, parameters]
+        """
+        if (shared_weight is None):
+            parameters = {}
+            for i in range(1, 14):
+                parameters["w_conv" + str(i)] = nn_utils.cnn_utils.weight_convolution_normal([3, 3], self.image_channel, 64, 0.1)
+                parameters["b_covn" + str(i)] = nn_utils.cnn_utils.bias_convolution(64, 0.0)
+        else:
+            parameters = shared_weight
