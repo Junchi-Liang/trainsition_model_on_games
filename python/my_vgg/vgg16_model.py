b2cd2ef590caec84e6a49844f0bf0053fa62a73b
Junchi-Liang <junchiliang@163.com>
2017-06-27 15:04:44 -0400

    constructing computation graph

--- python/my_vgg/vgg16_model.py
+++ python/my_vgg/vgg16_model.py
@@ -6,7 +6,7 @@ class VGG16_model:
     """
         My Implementation for VGG16
     """
-    def __init__(self, img_height, img_width, img_channel, training_batch_size = None, test_batch_size = None):
+    def __init__(self, img_height, img_width, img_channel, training_batch_size = None, test_batch_size = None, pretrained_weight = None):
         """
             img_height : int
             img_height = height of images
@@ -18,12 +18,21 @@ class VGG16_model:
             training_batch_size = size of training batch, when it is none, no training network is constructed
             test_batch_size : int
             test_batch_size = size of test batch, when it is none, no test network is constructed
+            pretrained_weight : dictionary
+            pretrained_weight = weights from a pretrained model
         """
         self.image_height = img_height
         self.image_width = img_width
         self.image_channel = img_channel
         self.train_batch_size = training_batch_size
         self.test_batch_size = test_batch_size
+        if (training_batch_size is not None):
+            self.train_net, self.parameters = construct_network_computation_graph(batch_size = training_batch_size, shared_weight = pretrained_weight)
+        if (test_batch_size is not None):
+            try:
+                self.test_net, self.parameters = construct_network_computation_graph(batch_size = test_batch_size, shared_weight = self.parameters)
+            except AttributeError:
+                self.test_net, self.parameters = construct_network_computation_graph(batch_size = test_batch_size, shared_weight = pretrained_weight)
 
     def construct_network_computation_graph(input_layer = None, batch_size = 0, shared_weight = None):
         """
