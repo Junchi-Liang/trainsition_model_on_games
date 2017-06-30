46fad33d9ae7df780f229ab3e841b6244201d9ab
Junchi-Liang <junchiliang@163.com>
2017-06-30 15:09:26 -0400

    FCN architecture

--- python/my_fcn/fcn_model.py
+++ python/my_fcn/fcn_model.py
@@ -19,18 +19,81 @@ class FCN_model:
                             i.e. changes of one model will only appear in this model
         """
 
-    def build_computation_graph(self, parameters, batch_size):
+    def build_computation_graph(self, img_height, img_width, parameters, batch_size, input_layer = None):
         """
             build computation graph for the FCN architecture
+            assume color images with RGB channels
+            img_height : int
+            img_height = height of images
+            img_width : int
+            img_width = width of images
             parameters : dictionary
             parameters = parameters used in this architecture
             batch_size : int
             batch_size = batch size for this graph
+            input_layer : tensorflow.python.framework.ops.Tensor
+            input_layer = input layer. If it is None, a new layer of placeholder will be constructed
             ------------------------------------------------
             return layers
             layers : dictionary
             layers = collection of tensors for each layers, indexed by name
         """
+        layers = []
+        if (input_layer is None):
+            layers["image_input"] = tf.placeholder(tf.float32, shape = [\
+                                       batch_size, img_height, img_width, 3])
+        else:
+            layers["image_input"] = input_layer
+        layers["conv1_1"] = tf.nn.bias_add(tf.nn.conv2d(layers["image_input"],
+                                       parameters["w_conv1_1"], strides=[1, 1, 1, 1], padding='SAME'), parameters["b_conv1_1"])
+        layers["relu1_1"] = tf.nn.relu(layers["conv1_1"])
+        layers["conv1_2"] = tf.nn.bias_add(tf.nn.conv2d(layers["relu1_1"],
+                                       parameters["w_conv1_2"], strides=[1, 1, 1, 1], padding='SAME'), parameters["b_conv1_2"])
+        layers["relu1_2"] = tf.nn.relu(layers["conv1_2"])
+        layers["pool1"] = tf.nn.max_pool(layers["relu1_2"], ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
+        layers["conv2_1"] = tf.nn.bias_add(tf.nn.conv2d(layers["pool1"], parameters["w_conv2_1"], strides = [1, 1 ,1, 1],\
+                                       padding = 'SAME'), parameters["b_conv2_1"])
+        layers["relu2_1"] = tf.nn.relu(layers["conv2_1"])
+        layers["conv2_2"] = tf.nn.bias_add(tf.nn.conv2d(layers["relu2_1"], parameters["w_conv2_2"], strides = [1, 1, 1, 1], \
+                                       padding = 'SAME'), parameters["b_conv2_2"])
+        layers["relu2_2"] = tf.nn.relu(layers["conv2_2"])
+        layers["pool2"] = tf.nn.max_pool(layers["relu2_2"], ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
+        layers["conv3_1"] = tf.nn.bias_add(tf.nn.conv2d(layers["pool2"], parameters["w_conv3_1"], strides = [1, 1, 1, 1], \
+                                       padding = 'SAME'), parameters["b_conv3_1"])
+        layers["relu3_1"] = tf.nn.relu(layers["conv3_1"])
+        layers["conv3_2"] = tf.nn.bias_add(tf.nn.conv2d(layers["relu3_1"], parameters["w_conv3_2"], strides = [1, 1, 1, 1], \
+                                       padding = 'SAME'), parameters["b_conv3_2"])
+        layers["relu3_2"] = tf.nn.relu(layers["conv3_2"])
+        layers["conv3_3"] = tf.nn.bias_add(tf.nn.conv2d(layers["relu3_2"], parameters["w_conv3_3"], strides = [1, 1, 1, 1], \
+                                       padding = 'SAME'), parameters["b_conv3_3"])
+        layers["relu3_3"] = tf.nn.relu(layers["conv3_3"])
+        layers["pool3"] = tf.nn.max_pool(layers["relu3_3"], ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
+        layers["conv4_1"] = tf.nn.bias_add(tf.nn.conv2d(layers["pool3"], parameters["w_conv4_1"], strides = [1, 1, 1, 1], \
+                                       padding = 'SAME'), parameters["b_conv4_1"])
+        layers["relu4_1"] = tf.nn.relu(layers["conv4_1"])
+        layers["conv4_2"] = tf.nn.bias_add(tf.nn.conv2d(layers["relu4_1"], parameters["w_conv4_2"], strides = [1, 1, 1, 1], \
+                                       padding = 'SAME'), parameters["b_conv4_2"])
+        layers["relu4_2"] = tf.nn.relu(layers["conv4_2"])
+        layers["conv4_3"] = tf.nn.bias_add(tf.nn.conv2d(layers["relu4_2"], parameters["w_conv4_3"], strides = [1, 1, 1, 1], \
+                                        padding = 'SAME'), parameters["b_conv4_3"])
+        layers["relu4_3"] = tf.nn.relu(layers["conv4_3"])
+        layers["pool4"] = tf.nn.max_pool(layers["relu4_3"], ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
+        layers["conv5_1"] = tf.nn.bias_add(tf.nn.conv2d(layers["pool4"], parameters["w_conv5_1"], strides = [1, 1, 1, 1], \
+                                        padding = 'SAME'), parameters["b_conv5_1"])
+        layers["relu5_1"] = tf.nn.relu(layers["conv5_1"])
+        layers["conv5_2"] = tf.nn.bias_add(tf.nn.conv2d(layers["relu5_1"], parameters["w_conv5_2"], strides = [1, 1, 1, 1], \
+                                        padding = 'SAME'), parameters["b_conv5_2"])
+        layers["relu5_2"] = tf.nn.relu(layers["conv5_2"])
+        layers["conv5_3"] = tf.nn.bias_add(tf.nn.conv2d(layers["relu5_2"], parameters["w_conv5_3"], strides = [1, 1, 1, 1], \
+                                        padding = 'SAME'), parameters["b_conv5_3"])
+        layers["relu5_3"] = tf.nn.relu(layers["conv5_3"])
+        layers["pool5"] = tf.nn.max_pool(layers["relu5_3"], ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
+        layers["conv6"] = tf.nn.bias_add(tf.nn.conv2d(layers["pool5"], parameters["w_conv6"], strides = [1, 7, 7, 1], \
+                                         padding = 'SAME'), parameters["b_conv6"])
+        layers["relu6"] = tf.nn.relu(layers["conv6"])
+        layers["conv7"] = tf.nn.bias_add(tf.nn.conv2d(layers["relu6"], parameters["w_conv7"], strides = [1, 1, 1, 1], \
+                                         padding = 'SAME'), parameters["b_conv7"])
+        layers["relu7"] = tf.nn.relu(layers["conv7"])
 
     def convert_VGG(self, vgg_model):
         """
